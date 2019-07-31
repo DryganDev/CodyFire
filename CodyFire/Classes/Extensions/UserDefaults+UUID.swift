678e35e8d62fcf74e6d57d7eaa421041b5d5fd98
Drygan <gigo12121212@gmail.com>
2019-07-31 22:33:21 +0300

    remove UIKit

--- CodyFire/Classes/Extensions/UserDefaults+UUID.swift
+++ CodyFire/Classes/Extensions/UserDefaults+UUID.swift
@@ -6,14 +6,13 @@
 //
 
 import Foundation
-import UIKit
 
 extension UserDefaults {
     static func uuid() -> String {
         if let uuid = UserDefaults.codyfire?.string(forKey: "uuid") {
             return uuid
         }
-        let uuid = UIDevice.current.identifierForVendor!.uuidString
+        let uuid = UUID().uuidString
         UserDefaults.codyfire?.setValue(uuid, forKey: "uuid")
         UserDefaults.codyfire?.synchronize()
         return uuid
